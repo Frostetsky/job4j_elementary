60106f88853c9073db3525449d755afec3f3fd8f
Frostetsky <nesterenko-nikita@bk.ru>
2020-06-03 13:57:02 +0300

    5.2. Создать программу, вычисляющую факториал. [#292411]

--- src/ru/job4j/loop/Factorial.java
+++ src/ru/job4j/loop/Factorial.java
@@ -3,13 +3,9 @@ package ru.job4j.loop;
 public class Factorial {
     public static int calc(int n) {
         int result = 1;
-        if (n == 0) return 1;
-        else if (n < 0) { System.out.println("Факториала от отрицательного числа не существует"); return n; }
-        else {
             for (int i = 1; i <= n; i++) {
                 result = result * i;
             }
-        }
         return result;
     }
 }
