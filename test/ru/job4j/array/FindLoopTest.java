71e9588f108776cbe41638235db8d3bc6441bac2
Frostetsky <nesterenko-nikita@bk.ru>
2020-06-04 15:14:05 +0300

    6.1. Классический поиск перебором. [#292327]

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -10,7 +10,7 @@ public class FindLoopTest {
     @Test
     public void whenArrayHas5Then0() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 3};
+        int[] input = new int[]{5, 10, 3};
         int value = 5;
         int result = find.indexOf(input, value);
         int expect = 0;
@@ -19,10 +19,19 @@ public class FindLoopTest {
 
     @Test
     public void whenArrayHas10Then4() {
-        int[] input = new int[] {1, 7, 8, 9, 10};
+        int[] input = new int[]{1, 7, 8, 9, 10};
         int value = 10;
-        int result = new FindLoop().indexOf(input,value);
+        int result = new FindLoop().indexOf(input, value);
         int expect = 4;
-        assertThat(result,is(expect));
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayHasNotThenNeg1() {
+        int[] input = new int[]{1, 7, 3, 9};
+        int value = 5;
+        int result = new FindLoop().indexOf(input, value);
+        int expect = -1;
+        assertThat(result, is(expect));
     }
 }
