ac09c55f56e9ce02a567c3bca897fde8a58c7370
Frostetsky <nesterenko-nikita@bk.ru>
2020-06-05 18:56:29 +0300

    6.6.1. Двухмерный массив. Вывод на консоль. [#292347]

---
+++ src/ru/job4j/array/BarleyBreak.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class BarleyBreak {
+    public static void main(String[] args) {
+        int[][] testArray = {{1, 2, 3},
+                             {4, 5, 6},
+                             {7, 8, 9}};
+        System.out.println(Arrays.deepToString(testArray));
+    }
+}
