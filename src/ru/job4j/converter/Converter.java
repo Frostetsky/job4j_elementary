4b1484f363f23bfb6b86e5dff5672a0eb103434b
Frostetsky <nesterenko-nikita@bk.ru>
2020-06-05 18:29:06 +0300

    5.8. JavaDoc. Code convention [#292413]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,16 +1,40 @@
 package ru.job4j.converter;
 
+/**
+ * Class Converter решение задачи блока "Базовый синтакс".
+ * @author Никита Нестеренко.
+ * @since 01.06.2020.
+ */
+
 public class Converter {
+
+    /**
+     * Конвертация из Рубля в Евро.
+     * @param value - значение в рублях.
+     * @return rtd - результат.
+     */
 
     public static int rubleToEuro(int value) {
         int rsl = value / 70;
         return rsl;
     }
 
+    /**
+     * Конвертация из Рубля в Доллары.
+     * @param value - значение в рублях.
+     * @return rtd - результат.
+     */
+
     public static int rubleToDollar(int value) {
         int rtd = value / 60;
         return rtd;
     }
+
+    /**
+     * Ручное тестирование (без использования Junit).
+     * Вывод подтверждения правильной конвертации в консоль.
+     * @param args
+     */
 
     public static void main(String[] args) {
         int in = 180;
