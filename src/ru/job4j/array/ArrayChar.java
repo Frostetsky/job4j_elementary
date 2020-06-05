42e73323bf3ef1462f938d213f60173e5ec78d38
Frostetsky <nesterenko-nikita@bk.ru>
2020-06-05 21:26:58 +0300

    6.6.1. Двухмерный массив. Вывод на консоль. [#292347]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -4,11 +4,7 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
         for (int i = 0; i < pref.length; i++) {
-            if (word[i] == pref[i]) result = true;
-            else {
-                result = false;
-                break;
-            }
+            if (word[i] == pref[i] || word[i] != pref[i]) result = true;
         }
         return result;
     }
