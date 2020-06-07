dcbe9a7e370a84d3c886d4544c3565725e1d231f
Frostetsky <nesterenko-nikita@bk.ru>
2020-06-07 14:30:39 +0300

    5.7. Ипотека. [#292404]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -4,11 +4,10 @@ public class Mortgage {
     public int year(int amount, int salary, double percent) {
         int year = 0;
         double x = 0;
-        while (amount > salary) {
-           x = amount * percent - salary + amount;
-           amount = (int) x;
-           year++;
-        }
+            while (amount >= 0) {
+                amount = (int) (amount * (percent / 100) - salary + amount);
+                year++;
+            }
         return year;
     }
 }
