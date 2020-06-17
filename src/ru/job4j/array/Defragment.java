28e86340b97fbe1a1a69b2aeb9abf4e383354b34
Frostetsky <nesterenko-nikita@bk.ru>
2020-06-17 21:36:23 +0300

    6.8. Дефрагментация массива. [#292348]

--- src/ru/job4j/array/Defragment.java
+++ src/ru/job4j/array/Defragment.java
@@ -2,18 +2,17 @@ package ru.job4j.array;
 
 public class Defragment {
     public static String[] compress(String[] array) {
+        int index2 = 0;
         for (int index1 = 0; index1 < array.length; index1++) {
-            for (int index2 = 0; index2 < array.length; index2++) {
-                if (array[index2] == null) {
-                    int point2 = index2;
-                    swap(array, index1, point2);
-                }
+            if (array[index1] == null) {
+                swap(array, index2, index1);
+                index2++;
             }
         }
         return array;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[]args) {
         String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
         String[] compressed = compress(input);
         for (int index = 0; index < compressed.length; index++) {
@@ -21,7 +20,7 @@ public class Defragment {
         }
     }
 
-    public static void swap(String[] array, int point1, int point2) {
+    public static void swap(String[] array,int point1, int point2) {
         String tmp = array[point1];
         array[point1] = array[point2];
         array[point2] = tmp;
---
+++ src/ru/job4j/io/MagicBall.java
@@ -0,0 +1,11 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class MagicBall {
+    public static void main(String[] args) {
+       // BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+       // System.out.print("Я великий Оракул. Что ты хочешь узнать? ");
+    }
+}
