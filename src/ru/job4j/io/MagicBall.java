550cd1cecc538817bc309ccfb97d0f32b4cb9713
Frostetsky <nesterenko-nikita@bk.ru>
2020-06-17 21:46:16 +0300

    1. Чтение из консоли. Класс Scanner. [#292451]

--- src/ru/job4j/io/MagicBall.java
+++ src/ru/job4j/io/MagicBall.java
@@ -1,11 +1,28 @@
 package ru.job4j.io;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class MagicBall {
-    public static void main(String[] args) {
-       // BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-       // System.out.print("Я великий Оракул. Что ты хочешь узнать? ");
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Я великий Оракул. Что ты хочешь узнать? ");
+        String question = reader.readLine();
+        switch (randomNumber()) {
+            case 0:
+                System.out.println("Да");
+                break;
+            case 1:
+                System.out.println("Нет");
+                break;
+            default :
+                System.out.println("Может быть");
+                break;
+        }
+    }
+
+    public static int randomNumber() {
+        return (int) (Math.random() * 3);
     }
 }
