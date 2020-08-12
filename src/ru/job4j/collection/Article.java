fae260d96bc77b1455f7c767648fc0e0aa7bd89a
Frostetsky <nesterenko-nikita@bk.ru>
2020-08-12 11:33:43 +0300

    Контрольные вопросы. [#292422]

--- src/ru/job4j/collection/Article.java
+++ src/ru/job4j/collection/Article.java
@@ -4,17 +4,16 @@ import java.util.*;
 
 public class Article {
     public static boolean generateBy(String origin, String line) {
-        char[] array = origin.toCharArray();
-        char[] array2 = line.toCharArray();
-        HashSet<Character> set = new HashSet<>();
-        for (char chari : array) {
-            set.add(chari);
+        boolean result = true;
+        String[] array = origin.split(" ?(?<!\\G)((?<=[^\\p{Punct}])(?=\\p{Punct})|\\b) ?");
+        String[] array2 = line.split(" ?(?<!\\G)((?<=[^\\p{Punct}])(?=\\p{Punct})|\\b) ?");
+        HashSet<String> set = new HashSet<>();
+        Collections.addAll(set,array);
+        for (int i = 0; i < array2.length; i++) {
+            if (!set.contains(array2[i])) {
+                result = false;
+            }
         }
-        HashSet<Character> set2 = new HashSet<>();
-        for (char chari : array2) {
-            set2.add(chari);
-        }
-        boolean result = set.containsAll(set2);
         return result;
     }
 }
