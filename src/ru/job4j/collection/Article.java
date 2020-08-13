d697768c30e856d05632ba8221e873e3d444619b
Frostetsky <nesterenko-nikita@bk.ru>
2020-08-13 11:22:07 +0300

    Контрольные вопросы. [#292422]

--- src/ru/job4j/collection/Article.java
+++ src/ru/job4j/collection/Article.java
@@ -5,8 +5,9 @@ import java.util.*;
 public class Article {
     public static boolean generateBy(String origin, String line) {
         boolean result = true;
-        String[] array = origin.split(" ?(?<!\\G)((?<=[^\\p{Punct}])(?=\\p{Punct})|\\b) ?");
-        String[] array2 = line.split(" ?(?<!\\G)((?<=[^\\p{Punct}])(?=\\p{Punct})|\\b) ?");
+        String neworigin = origin.replaceAll( "\\p{Punct}","");
+        String[] array = neworigin.split(" ");
+        String[] array2 = line.split(" ");
         HashSet<String> set = new HashSet<>();
         Collections.addAll(set,array);
         for (int i = 0; i < array2.length; i++) {
