3b4866851926b736bddd0c4c3f9dc73cbc00501e
Frostetsky <nesterenko-nikita@bk.ru>
2020-08-15 11:37:04 +0300

    0. Stream API [#292293]

---
+++ src/ru/job4j/stream/FirstStream.java
@@ -0,0 +1,20 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FirstStream {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(5);
+        numbers.add(-1);
+        numbers.add(0);
+        numbers.add(10);
+        numbers.add(9);
+        List<Integer> sortnumbers = numbers.stream()
+                                           .filter(x -> x > 0)
+                                           .collect(Collectors.toList());
+        sortnumbers.forEach(System.out::println);
+    }
+}
