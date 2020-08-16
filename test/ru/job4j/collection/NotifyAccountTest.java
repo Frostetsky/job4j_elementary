cdc23df6489ced6ece8aa02063f8d4ade28f2b65
Frostetsky <nesterenko-nikita@bk.ru>
2020-08-16 12:56:46 +0300

    1. Улучшения Collection API [#292304]

--- test/ru/job4j/collection/NotifyAccountTest.java
+++ test/ru/job4j/collection/NotifyAccountTest.java
@@ -28,7 +28,7 @@ public class NotifyAccountTest {
 
     @Test
     public void haveDuplicates() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001"),
                 new Account("155", "John MacTavish", "032451"),
